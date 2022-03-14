f22ec6337b22e71a84b6df4411dcc1659d3d9651
RaphaelTetreault <raphael.tetreault@outlook.com>
2022-03-14 14:20:06 -0400

    Copy GameCube.GFZ

---
+++ src/GameCube.GFZ/LZ/FileStatus.cs
@@ -0,0 +1,24 @@
+﻿namespace GameCube.GFZ.LZ
+{
+    /// <summary>
+    /// Metadata struct to indicate file status after IO operation.
+    /// </summary>
+    public struct FileStatus
+    {
+        /// <summary>
+        /// Indicates the IO operation for was successful.
+        /// </summary>
+        public bool success;
+
+        /// <summary>
+        /// The file path of the IO operation.
+        /// </summary>
+        public string filePath;
+
+        public FileStatus(bool success, string filePath)
+        {
+            this.success = success;
+            this.filePath = filePath;
+        }
+    }
+}
