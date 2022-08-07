e4bd224c2d3d1415c7f7ebba012dcf48f60ac84b
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-08-07 00:27:10 -0400

    Try a few edits to CheckpointGrid - appears to be used for dynamic camera?

--- src/GameCube.GFZ/Stage/CheckpointGrid.cs
+++ src/GameCube.GFZ/Stage/CheckpointGrid.cs
@@ -14,7 +14,7 @@ namespace GameCube.GFZ.Stage
         // CONSTANTS
         public const int Subdivisions = 8;
         public const int kListCount = Subdivisions * Subdivisions;
-        
+
         // PROPERTIES
         public override int SubdivisionsX => Subdivisions;
         public override int SubdivisionsZ => Subdivisions;
@@ -43,7 +43,7 @@ namespace GameCube.GFZ.Stage
             bounds.NumSubdivisionsX = Subdivisions;
             bounds.NumSubdivisionsZ = Subdivisions;
             bounds.Left = min.x;
-            bounds.Top = max.z;
+            bounds.Top = min.z;
             bounds.SubdivisionWidth = (max.x - min.x) / Subdivisions; // delta / subdivisions
             bounds.SubdivisionLength = (max.z - min.z) / Subdivisions; // delta / subdivisions
 
@@ -51,7 +51,7 @@ namespace GameCube.GFZ.Stage
         }
 
         // METHODS
-        public void GenerateIndexes(GridXZ matrixBoundsXZ, Checkpoint[] checkpoints)
+        public void GenerateIndexesOld(GridXZ matrixBoundsXZ, Checkpoint[] checkpoints)
         {
             // Init. Value is from inherited structure.
             IndexLists = new IndexList[kListCount];
@@ -79,19 +79,19 @@ namespace GameCube.GFZ.Stage
             // Iterate over each subdivision in the course
             for (int z = 0; z < SubdivisionsZ; z++)
             {
-                // Get the minimum and maximum Z coordinates allowed to exist this cell
+                // Get the minimum and maximum Z coordinates allowed to exist in this cell
                 var minZIndex = math.clamp(z - 2, 0, SubdivisionsZ - 1);
                 var maxZIndex = math.clamp(z + 2, 0, SubdivisionsZ - 1);
-                var minZ = matrixBoundsXZ.Top - (lengthZ * minZIndex);
-                var maxZ = matrixBoundsXZ.Top - (lengthZ * maxZIndex);
+                var minZ = matrixBoundsXZ.Top + (lengthZ * minZIndex);
+                var maxZ = matrixBoundsXZ.Top + (lengthZ * maxZIndex);
 
                 for (int x = 0; x < SubdivisionsX; x++)
                 {
                     // Get the minimum and maximum X coordinates allowed to exist this cell
                     var minXIndex = math.clamp(x - 2, 0, SubdivisionsX - 1);
                     var maxXIndex = math.clamp(x + 2, 0, SubdivisionsX - 1);
-                    var minX = matrixBoundsXZ.Left - (widthX * minXIndex);
-                    var maxX = matrixBoundsXZ.Left - (widthX * maxXIndex);
+                    var minX = matrixBoundsXZ.Left + (widthX * minXIndex);
+                    var maxX = matrixBoundsXZ.Left + (widthX * maxXIndex);
 
                     // Iterate over every checkpoint the course has
                     var indexes = new List<int>();
@@ -128,5 +128,82 @@ namespace GameCube.GFZ.Stage
             return isBetween;
         }
 
+        public void GenerateIndexesBlanks(GridXZ matrixBoundsXZ, Checkpoint[] checkpoints)
+        {
+            var list = new List<int>();
+            for (int i = 0; i < checkpoints.Length; i++)
+                list.Add(i);
+
+            for (int i = 0; i < IndexLists.Length; i++)
+                IndexLists[i] = IndexList.CreateIndexList(list);
+        }
+
+
+        public void GenerateIndexesBetter(GridXZ matrixBoundsXZ, Checkpoint[] checkpoints)
+        {
+            //
+            var widthX = matrixBoundsXZ.SubdivisionWidth;
+            var lengthZ = matrixBoundsXZ.SubdivisionLength;
+            // Iterate over each subdivision in the course
+            for (int z = 0; z < SubdivisionsZ; z++)
+            {
+                // Get the minimum and maximum Z coordinates allowed to exist in this cell
+                var minZIndex = math.clamp(z - 1, 0, SubdivisionsZ - 1);
+                var maxZIndex = math.clamp(z + 1, 0, SubdivisionsZ - 1);
+                var minZ = matrixBoundsXZ.Top + (lengthZ * minZIndex);
+                var maxZ = matrixBoundsXZ.Top + (lengthZ * maxZIndex);
+
+                for (int x = 0; x < SubdivisionsX; x++)
+                {
+                    // Get the minimum and maximum X coordinates allowed to exist in this cell
+                    var minXIndex = math.clamp(x - 1, 0, SubdivisionsX - 1);
+                    var maxXIndex = math.clamp(x + 1, 0, SubdivisionsX - 1);
+                    var minX = matrixBoundsXZ.Left + (widthX * minXIndex);
+                    var maxX = matrixBoundsXZ.Left + (widthX * maxXIndex);
+
+                    // Iterate over every checkpoint the course has
+                    var indexes = new List<int>();
+                    for (int i = 0; i < checkpoints.Length; i++)
+                    {
+                        var checkpoint = checkpoints[i];
+                        var start = checkpoint.PlaneStart.origin;
+                        var end = checkpoint.PlaneEnd.origin;
+
+                        bool intersects = line_rect_isect(start.x, start.z, end.x, end.z, minX, maxX, minZ, maxZ);
+                        if (intersects)
+                            indexes.Add(i);
+
+                        //bool minAboveMaxZ = start.z > maxZ && end.z > maxZ;
+                        //bool maxBelowMinZ = start.z < minZ && end.z < minZ;
+                        //bool maxLeftOfMinX = start.x < minX && end.x < minX;
+                        //bool minRightOfMaxX = start.x > maxX && end.x > maxX;
+                        //bool doesNotIntersect = minAboveMaxZ || maxBelowMinZ || maxLeftOfMinX || minRightOfMaxX;
+                        //if (doesNotIntersect)
+                        //    continue;
+                    }
+
+                    // Turn those indexes into the structure
+                    var cell = z * SubdivisionsZ + x;
+                    IndexLists[cell] = IndexList.CreateIndexList(indexes);
+                }
+            }
+
+
+
+        }
+
+        //https://www.lexaloffle.com/bbs/?pid=80455
+        private bool line_rect_isect(float startX, float startZ, float endX, float endZ, float minX, float maxX, float minZ, float maxZ)
+        {
+            float tl = (minX - startX) / (endX - startX);
+            float tr = (maxX - startX) / (endX - startX);
+            float tt = (maxZ - startZ) / (endZ - startZ);
+            float tb = (minZ - startZ) / (endZ - startZ);
+
+            bool intersects = 
+                math.max(0, math.max(math.min(tl, tr), math.min(tt, tb))) <
+                math.min(1, math.min(math.max(tl, tr), math.max(tt, tb)));
+            return intersects;
+        }
     }
 }
