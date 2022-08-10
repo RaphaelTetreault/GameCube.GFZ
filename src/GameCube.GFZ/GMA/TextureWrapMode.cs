656acd2621fa535ea6ca49b8e28de8be4bafd838
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2022-08-09 22:46:52 -0400

    m: name known NBT flag

--- src/GameCube.GFZ/GMA/TextureWrapMode.cs
+++ src/GameCube.GFZ/GMA/TextureWrapMode.cs
@@ -7,7 +7,7 @@ namespace GameCube.GFZ.GMA
     public enum TextureWrapMode : byte
     {
         unk0 = 1 << 0,
-        unk1 = 1 << 1,
+        UseNBT = 1 << 1,
         repeatX = 1 << 2,
         mirrorX = 1 << 3,
         repeatY = 1 << 4,
