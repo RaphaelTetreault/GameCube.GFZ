7a81e64a59952a91ac16461f2e8d2a7e90093d14
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2023-01-28 00:06:26 -0500

    Add initial implementation of Emblem files

---
+++ src/GameCube.GFZ.Emblem/Emblem.cs
@@ -0,0 +1,14 @@
+﻿using GameCube.GX;
+using GameCube.GX.Texture;
+using Manifold;
+using Manifold.IO;
+
+namespace GameCube.GFZ.Emblem
+{
+    public class Emblem : FixedSizeTextureRGB5A3
+    {
+        public override int Width => 64;
+        public override int Height => 64;
+        public static int Size => 64 * 64 * sizeof(ushort);
+    }
+}
---
+++ src/GameCube.GFZ.Emblem/EmblemBIN.cs
@@ -0,0 +1,42 @@
+﻿using GameCube.GX;
+using GameCube.GX.Texture;
+using Manifold;
+using Manifold.IO;
+
+namespace GameCube.GFZ.Emblem
+{
+    public class EmblemBIN :
+        IBinaryFileType,
+        IBinarySerializable
+    {
+        public const Endianness endianness = Endianness.BigEndian;
+
+        public Endianness Endianness => endianness;
+        public string FileExtension => ".bin";
+        public string FileName { get; set; } = string.Empty;
+
+
+        private Emblem[] emblems = new Emblem[0];
+
+        public Emblem[] Emblems => emblems;
+
+
+        public void Deserialize(EndianBinaryReader reader)
+        {
+            bool isValidFileSize = (int)(reader.BaseStream.Length % Emblem.Size) == 0;
+            if (!isValidFileSize)
+            {
+                string msg = $"File is not an exact multiple of 0x{Emblem.Size:x4}.";
+                throw new ArgumentException(msg);
+            }
+
+            int count = (int)(reader.BaseStream.Length / Emblem.Size);
+            reader.Read(ref emblems, count);
+        }
+
+        public void Serialize(EndianBinaryWriter writer)
+        {
+            writer.Write(emblems);
+        }
+    }
+}
