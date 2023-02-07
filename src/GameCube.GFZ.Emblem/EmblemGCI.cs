57f6eac76c8e82bac793f1a4511ea0cd19b8559a
Raphaël Tétreault <raphaeltetreault@hotmail.com>
2023-02-07 00:54:05 -0500

    Add GameCode (eg: GFZJ01) enum, add EmblemGCI utility functions

--- src/GameCube.GFZ.Emblem/EmblemGCI.cs
+++ src/GameCube.GFZ.Emblem/EmblemGCI.cs
@@ -1,4 +1,5 @@
-﻿using GameCube.GX;
+﻿using GameCube.GFZ;
+using GameCube.GX;
 using GameCube.GX.Texture;
 using Manifold;
 using Manifold.IO;
@@ -18,8 +19,11 @@ namespace GameCube.GFZ.Emblem
     {
         public const Endianness endianness = Endianness.BigEndian;
         public const string Extension = ".gci";
+        public const string GameCodeGFZE01 = "GFZE01";
+        public const string GameCodeGFZJ01 = "GFZJ01";
+        public const string GameCodeGFZP01 = "GFZP01";
         public const int GameCodeLength = 6;
-        public const int FileNameLength = 32;
+        public const int InternalFileNameLength = 32;
         public const int GameTitleLength = 32;
         public const int CommentLength = 60;
         public const int FileSize = 24_640; // bytes
@@ -34,7 +38,7 @@ namespace GameCube.GFZ.Emblem
         private byte const_0xFF;
         private byte regionCode;
         private string internalFileName = "";
-        private int timestamp;
+        private uint timestamp;
         private byte[] unkData = new byte[9];
         private byte copyCount;
         private ushort memoryCardStartBlock;
@@ -42,7 +46,7 @@ namespace GameCube.GFZ.Emblem
         private int unk1;
         private ushort checksum;
         private ushort const_0x0401;
-        private string gameTitle = "";
+        private string gameTitle = "F-ZERO GX";
         private string comment = "";
         private MenuBanner banner = new MenuBanner();
         private MenuIcon icon = new MenuIcon();
@@ -53,7 +57,7 @@ namespace GameCube.GFZ.Emblem
         public byte Const_0xFF { get => const_0xFF; set => const_0xFF = value; }
         public byte RegionCode { get => regionCode; set => regionCode = value; }
         public string InternalFileName { get => internalFileName; set => internalFileName = value; }
-        public int Timestamp { get => timestamp; set => timestamp = value; }
+        public uint Timestamp { get => timestamp; set => timestamp = value; }
         public byte[] UnkData { get => unkData; set => unkData = value; }
         public byte CopyCount { get => copyCount; set => copyCount = value; }
         public ushort MemoryCardStartBlock { get => memoryCardStartBlock; set => memoryCardStartBlock = value; }
@@ -63,10 +67,9 @@ namespace GameCube.GFZ.Emblem
         public ushort Const_0x0401 { get => const_0x0401; set => const_0x0401 = value; }
         public string GameTitle { get => gameTitle; set => gameTitle = value; }
         public string Comment { get => comment; set => comment = value; }
-        public MenuBanner Banner => banner;
-        public MenuIcon Icon => icon;
-        public Emblem Emblem => emblem;
-
+        public MenuBanner Banner { get => banner; set => banner = value; }
+        public MenuIcon Icon { get => icon; set => icon = value; }
+        public Emblem Emblem { get => emblem; set => emblem = value; }
 
         public void Deserialize(EndianBinaryReader reader)
         {
@@ -80,7 +83,7 @@ namespace GameCube.GFZ.Emblem
             reader.Read(ref gameCode, TextEncoding, GameCodeLength);
             reader.Read(ref const_0xFF);
             reader.Read(ref regionCode);
-            reader.Read(ref internalFileName, TextEncoding, FileNameLength);
+            reader.Read(ref internalFileName, TextEncoding, InternalFileNameLength);
             reader.Read(ref timestamp);
             reader.Read(ref unkData, 9);
             reader.Read(ref copyCount);
@@ -96,11 +99,15 @@ namespace GameCube.GFZ.Emblem
             reader.Read(ref icon);
             reader.Read(ref emblem);
         }
-
         public void Serialize(EndianBinaryWriter writer)
         {
+            DateTime dateTime = DateTime.Now;
+            SetDefaultComment(dateTime, false);
+            SetTimestamp(dateTime);
+            checksum = ComputeCRC();
+
             Assert.IsTrue(gameCode.Length == GameCodeLength);
-            Assert.IsTrue(internalFileName.Length <= FileNameLength);
+            Assert.IsTrue(internalFileName.Length <= InternalFileNameLength);
             Assert.IsTrue(gameTitle.Length <= GameTitleLength);
             Assert.IsTrue(comment.Length <= CommentLength);
             Assert.IsTrue(unkData.Length <= 9);
@@ -109,7 +116,7 @@ namespace GameCube.GFZ.Emblem
             writer.Write(0xFF);
             writer.Write(regionCode);
             writer.Write(internalFileName, TextEncoding, false);
-            writer.WritePadding(0x00, FileNameLength - internalFileName.Length);
+            writer.WritePadding(0x00, InternalFileNameLength - internalFileName.Length);
             writer.Write(timestamp);
             writer.Write(unkData);
             writer.Write(copyCount);
@@ -127,5 +134,96 @@ namespace GameCube.GFZ.Emblem
             writer.Write(icon);
             writer.Write(emblem);
         }
+
+        public void SetRegionCode(GameCode gameCode)
+        {
+            switch (gameCode)
+            {
+                case GFZ.GameCode.GFZE01:
+                    regionCode = 1;
+                    GameCode = GameCodeGFZE01;
+                    return;
+
+                case GFZ.GameCode.GFZJ01:
+                    regionCode = 0;
+                    GameCode = GameCodeGFZJ01;
+                    return;
+
+                case GFZ.GameCode.GFZP01:
+                    regionCode = 2;
+                    GameCode = GameCodeGFZJ01;
+                    return;
+
+                case GFZ.GameCode.GFZJ8P:
+                    throw new ArgumentException("AX is not supported");
+
+                default:
+                    throw new ArgumentException($"Unhandled region code {gameCode}");
+            }
+        }
+        private void SetDefaultComment(DateTime dateTime, bool overwriteComment = false)
+        {
+            bool hasComment = !string.IsNullOrEmpty(comment);
+            if (hasComment || !overwriteComment)
+                return;
+
+            string time = dateTime.ToString("yyyy/MM/dd hh:mm.ss");
+            string name = System.Reflection.Assembly.GetEntryAssembly().GetName().Name;
+            comment = $"Created by {name} at {time}.";
+        }
+        private void SetTimestamp(DateTime dateTime)
+        {
+            DateTimeOffset dateTimeOffset = dateTime;
+            uint unixTime = (uint)dateTimeOffset.ToUnixTimeSeconds();
+            timestamp = unixTime;
+        }
+        public ushort ComputeCRC()
+        {
+            return 0xFCDC;
+        }
+        public bool SafeSetInternalFileName(string internalFileName)
+        {
+            // Trim file name if it is too long
+            bool fileNameFits = internalFileName.Length <= InternalFileNameLength;
+            if (!fileNameFits)
+            {
+                internalFileName = internalFileName.Substring(0, InternalFileNameLength);
+            }
+
+            // Set file name
+            InternalFileName = internalFileName;
+
+            // provide info
+            return fileNameFits;
+        }
+        public static string GetFileName(string fileName, GameCode gameCode)
+        {
+            const int maxChars = 20;
+            string fileName20Char = fileName.PadLeft(maxChars, '-');
+            fileName20Char = fileName20Char.Substring(0, maxChars);
+
+            char regionChar = GetRegionChar(gameCode);
+            char R = char.ToUpper(regionChar);
+            char r = char.ToLower(regionChar);
+
+            string name = $"8P-GFZ{R}-fz{r}020-{fileName20Char}";
+            return name;
+        }
+        private static char GetRegionChar(GameCode gameCode)
+        {
+            switch (gameCode)
+            {
+                case GFZ.GameCode.GFZE01: return 'e';
+                case GFZ.GameCode.GFZJ01: return 'j';
+                case GFZ.GameCode.GFZP01: return 'p';
+
+                case GFZ.GameCode.GFZJ8P:
+                    throw new ArgumentException("AX is not supported");
+
+                default:
+                    throw new ArgumentException($"Unhandled region code {gameCode}");
+            }
+        }
+
     }
 }
---
+++ src/GameCube.GFZ/GameCode.cs
@@ -0,0 +1,10 @@
+﻿namespace GameCube.GFZ
+{
+    public enum GameCode
+    {
+        GFZE01,
+        GFZJ01,
+        GFZP01,
+        GFZJ8P,
+    }
+}
