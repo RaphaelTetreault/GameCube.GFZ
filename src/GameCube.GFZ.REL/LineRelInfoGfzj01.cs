2f206a042f09ddb59728dfaa2e6b6025718334b3
Raphaël Tétreault <150467892+MohawkRaphaelT@users.noreply.github.com>
2023-12-21 00:36:23 -0500

    Add missing LineREL addresses for E and P versions

--- src/GameCube.GFZ.REL/LineRelInfoGfzj01.cs
+++ src/GameCube.GFZ.REL/LineRelInfoGfzj01.cs
@@ -35,26 +35,27 @@ namespace GameCube.GFZ.LineREL
 
         // COURSE NAMES
         public override int CourseNameLanguages => 6;
-        public override ArrayPointer32 CourseNameOffsets => new(0x1F286C, 666); // 
-        public override DataBlock CourseNamesEnglish => new DataBlock(0x19523C, 0x15C); // English strings
-        public override DataBlock CourseNamesLocalizations => new DataBlock(0x19555C, 0x8D8); // Other localization strings
+        public override ArrayPointer32 CourseNameOffsets => new(0x1F286C, 666); // 111 * 6
+        public override DataBlock CourseNamesEnglish => new(0x19523C, 0x15C); // English strings
+        public override DataBlock CourseNamesLocalizations => new(0x19555C, 0x8D8); // Other localization strings
 
+        // CARDATA
         public override Pointer CarDataMachinesPtr => 0x192140;
         public override Pointer MachineLetterRatingsPtr => 0x1AA3F8;
 
-        public override DataBlock CourseVenueIndex => new DataBlock(0x1951B4, 111);
-        public override DataBlock CourseDifficulty => new DataBlock(0x165460, 111);
-        public override DataBlock CourseBgmIndex => new DataBlock(0x1607B4, 56);
-        public override DataBlock CourseBgmFinalLapIndex => new DataBlock(0x1607EC, 184);
-        public override DataBlock CupCourseLut => new DataBlock(0x164548, 0x84);
-        public override DataBlock CupCourseLutAssets => new DataBlock(0x1645CC, 0x84);
-        public override DataBlock CupCourseLutUnk => new DataBlock(0x164650, 0x84);
-        public override DataBlock CourseMinimapParameterStructs => new DataBlock(0x188098, 0x508);
-        public override DataBlock ForbiddenWords => new DataBlock(0x1ABA60, 0x3E0);
-        public override DataBlock AxModeCourseTimers => new DataBlock(0x1A9390, 6);
-        public override List<CustomizableArea> CourseNameAreas => new List<CustomizableArea>();
-        public override DataBlock PilotPositions => new DataBlock(0x19E49C, 0x210);
-        public override DataBlock PilotToMachineLut => new DataBlock(0x164498, 0xA4);
+        public override DataBlock CourseVenueIndex => new(0x1951B4, 111);
+        public override DataBlock CourseDifficulty => new(0x165460, 111);
+        public override DataBlock CourseBgmIndex => new(0x1607B4, 56);
+        public override DataBlock CourseBgmFinalLapIndex => new(0x1607EC, 184);
+        public override DataBlock CupCourseLut => new(0x164548, 0x84);
+        public override DataBlock CupCourseLutAssets => new(0x1645CC, 0x84);
+        public override DataBlock CupCourseLutUnk => new(0x164650, 0x84);
+        public override DataBlock CourseMinimapParameterStructs => new(0x188098, 0x508);
+        public override DataBlock ForbiddenWords => new(0x1ABA60, 0x3E0);
+        public override DataBlock AxModeCourseTimers => new(0x1A9390, 6);
+        public override List<CustomizableArea> CourseNameAreas => new();
+        public override DataBlock PilotPositions => new(0x19E49C, 0x210);
+        public override DataBlock PilotToMachineLut => new(0x164498, 0xA4);
 
         public override short Salt => unchecked((short)0x0ce0);
         public override int Key0 => unchecked((int)0x0004f107);
