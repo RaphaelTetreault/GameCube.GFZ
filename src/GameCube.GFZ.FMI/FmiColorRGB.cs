e3d4960edb199a024fc7af5dce8c2fe1963ae9bb
Raphaël Tétreault <raphael.tetreault@outlook.com>
2024-04-21 14:38:27 -0400

    Fix deserialization issues with FMI

---
+++ src/GameCube.GFZ.FMI/FmiColorRGB.cs
@@ -0,0 +1,31 @@
+﻿using Manifold.IO;
+
+namespace GameCube.GFZ.FMI
+{
+    public struct FmiColorRGB :
+        IBinarySerializable
+    {
+        public float r;
+        public float g;
+        public float b;
+
+        public void Deserialize(EndianBinaryReader reader)
+        {
+            reader.Read(ref r);
+            reader.Read(ref g);
+            reader.Read(ref b);
+        }
+
+        public void Serialize(EndianBinaryWriter writer)
+        {
+            writer.Write(r);
+            writer.Write(g);
+            writer.Write(b);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(FmiColorRGB)}({r}, {g}, {b})";
+        }
+    }
+}
---
+++ src/GameCube.GFZ.FMI/FmiFile.cs
@@ -0,0 +1,23 @@
+﻿using Manifold.IO;
+using System.Text.Json.Serialization;
+
+namespace GameCube.GFZ.FMI
+{
+    public class FmiFile : BinaryFileWrapper<Fmi>
+    {
+        [JsonIgnore]
+        public const Endianness endianness = Endianness.BigEndian;
+
+        [JsonIgnore]
+        public override Endianness Endianness => endianness;
+
+        [JsonIgnore]
+        public override string FileExtension => ".fmi";
+
+        [JsonIgnore]
+        public override string FileName { get; set; } = string.Empty;
+
+        [JsonIgnore]
+        public override string Version { get; } = "1";
+    }
+}
