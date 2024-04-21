bcb53a8e92c1f34aca15115c03a279d1b06f4d8d
Raphaël Tétreault <raphael.tetreault@outlook.com>
2024-04-21 14:38:29 -0400

    Add missing LineREL addresses for E and P versions

--- src/GameCube.GFZ.REL/LineRelInfoGfzp01.cs
+++ src/GameCube.GFZ.REL/LineRelInfoGfzp01.cs
@@ -24,33 +24,33 @@ namespace GameCube.GFZ.LineREL
         public override string FileHashMD5 => kFileHashMD5;
         public override Pointer StringTableBaseAddress => 0x16E5A0;
 
-        public override ArrayPointer32 VenueNameOffsets => throw new System.NotImplementedException();
-        public override ArrayPointer32 VenueNamesEnglishOffsets => throw new System.NotImplementedException();
-        public override ArrayPointer32 VenueNamesJapaneseOffsets => throw new System.NotImplementedException();
-        public override DataBlock VenueNamesEnglish => new DataBlock(0x199940, 0xA0);
-        public override DataBlock VenueNamesJapanese => throw new System.NotImplementedException();
+        public override ArrayPointer32 VenueNameOffsets => new(0x201664, 42);
+        public override ArrayPointer32 VenueNamesEnglishOffsets => new(0x201664, 22); // Strings at: 199940
+        public override ArrayPointer32 VenueNamesJapaneseOffsets => new(0x201714, 20); // Strings at: 199A3C
+        public override DataBlock VenueNamesEnglish => new(0x199940, 0xA0);
+        public override DataBlock VenueNamesJapanese => new(0x199A3C, 0xD8);
 
-        public override int CourseNameLanguages => throw new System.NotImplementedException();
-        public override ArrayPointer32 CourseNameOffsets => new(0x201F38, 666);
+        public override int CourseNameLanguages => 6;
+        public override ArrayPointer32 CourseNameOffsets => new(0x201F34, 666); // 111 * 6
         public override DataBlock CourseNamesEnglish => new(0x19A11C, 0x15C);
-        public override DataBlock CourseNamesLocalizations => throw new System.NotImplementedException("This is absent from the EUR version");
+        public override DataBlock CourseNamesLocalizations => new(0x19A430, 0x1E4); // Japanese only (no GER, FRE, SPA, ITA)
 
-        public override Pointer CarDataMachinesPtr => throw new System.NotImplementedException();
+        public override Pointer CarDataMachinesPtr => 0x195648;
         public override Pointer MachineLetterRatingsPtr => 0x1B8E20;
 
-        public override DataBlock CourseVenueIndex => new DataBlock(0x19A094, 111);
-        public override DataBlock CourseDifficulty => new DataBlock(0x1698EC, 111);
-        public override DataBlock CourseBgmIndex => new DataBlock(0x16495C, 56);
-        public override DataBlock CourseBgmFinalLapIndex => new DataBlock(0x164994, 184);
-        public override DataBlock CupCourseLut => new DataBlock(0x1688B0, 0x84);
-        public override DataBlock CupCourseLutAssets => new DataBlock(0x168934, 0x84);
-        public override DataBlock CupCourseLutUnk => new DataBlock(0x1689B8, 0x84);
-        public override DataBlock CourseMinimapParameterStructs => new DataBlock(0x18CF50, 0x508);
-        public override DataBlock ForbiddenWords => new DataBlock(0x1BB83C, 0x3E0);
-        public override DataBlock AxModeCourseTimers => new DataBlock(0x1B7810, 6);
-        public override List<CustomizableArea> CourseNameAreas => new List<CustomizableArea>();
-        public override DataBlock PilotPositions => new DataBlock(0x1A38F4, 0x210);
-        public override DataBlock PilotToMachineLut => new DataBlock(0x168800, 0xA4);
+        public override DataBlock CourseVenueIndex => new(0x19A094, 111);
+        public override DataBlock CourseDifficulty => new(0x1698EC, 111);
+        public override DataBlock CourseBgmIndex => new(0x16495C, 56);
+        public override DataBlock CourseBgmFinalLapIndex => new(0x164994, 184);
+        public override DataBlock CupCourseLut => new(0x1688B0, 0x84);
+        public override DataBlock CupCourseLutAssets => new(0x168934, 0x84);
+        public override DataBlock CupCourseLutUnk => new(0x1689B8, 0x84);
+        public override DataBlock CourseMinimapParameterStructs => new(0x18CF50, 0x508);
+        public override DataBlock ForbiddenWords => new(0x1BB83C, 0x3E0);
+        public override DataBlock AxModeCourseTimers => new(0x1B7810, 6);
+        public override List<CustomizableArea> CourseNameAreas => new();
+        public override DataBlock PilotPositions => new(0x1A38F4, 0x210);
+        public override DataBlock PilotToMachineLut => new(0x168800, 0xA4);
 
         // Same as GFZE01
         public override short Salt => unchecked((short)0x180a);
