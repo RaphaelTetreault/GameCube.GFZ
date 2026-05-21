70e4bc40ff9c4828459b42574255ab6185769855
Raphaël Tétreault <41877613+RaphaelTetreault@users.noreply.github.com>
2026-05-21 01:15:48 -0400

    m: Fix BoneIndices8 (go back to array format)

--- src/GameCube.GFZ.GMA/BoneIndexes8.cs
+++ src/GameCube.GFZ.GMA/BoneIndexes8.cs
@@ -6,19 +6,25 @@ namespace GameCube.GFZ.GMA;
 /// <summary>
 ///     A set of 8 matrix indexes. Root indexes read as -1/0xFF, indicating no parent bone/matrix.
 /// </summary>
-[InlineArray(8)]
-public struct BoneIndexes8 :
+//[InlineArray(8)]
+public class BoneIndexes8 :
     IBinarySerializable
 {
+    const int Size = 8;
+
     // FIELDS
-    public sbyte boneIndex = -1;
+    //public sbyte boneIndex = -1;
+    public sbyte[] boneIndex = [-1, -1, -1, -1, -1, -1, -1, -1];
 
     // CONSTRUCTOR
     // Ensures default value is always -1.
     public BoneIndexes8() { }
 
     // METHODS
-    public void Deserialize(EndianBinaryReader reader) => reader.Read(ref boneIndex);
-    public readonly void Serialize(EndianBinaryWriter writer) => writer.Write(boneIndex);
+    //public void Deserialize(EndianBinaryReader reader) => reader.Read(ref boneIndex);
+    //public readonly void Serialize(EndianBinaryWriter writer) => writer.Write(boneIndex);
+
+    public void Deserialize(EndianBinaryReader reader) => reader.Read(ref boneIndex, Size);
+    public void Serialize(EndianBinaryWriter writer) => writer.Write(boneIndex);
 
 }
